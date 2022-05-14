77566a7b0132c97f73882419790e4f602887b01a
Alexei Semenenko <aleksei.syemyenenko@gmail.com>
2022-05-15 00:45:01 +0300

    added main logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,146 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { TASKS } from './constants'
+
 import './App.css'
 
+const totalLength = TASKS.map(it => it.questions).flat().length
+const emptyArray = Array.from(Array(totalLength))
+
 function App() {
+  const [answers, setAnswers] = useState<Array<any>>(emptyArray)
+  const [isAnswersVisible, setIsAnswersVisible] = useState<boolean>(false)
+  const [isCheckClicked, setIsCheckClick] = useState<boolean>(false)
+
+  function handleSetChoice(e: any, index: number) {
+    const newAnswers = [...answers]
+    newAnswers[index] = e.target.value
+
+    setAnswers(newAnswers)
+  }
+
+  function handleCheckAnswers() {
+    if (checkIfAllSelected()) {
+      setIsAnswersVisible(true)
+    } else {
+      setIsCheckClick(true)
+      alert('Please answer all questions')
+    }
+
+  }
+
+  function handleRestart() {
+    setAnswers(emptyArray)
+    setIsAnswersVisible(false)
+    setIsCheckClick(false)
+  }
+
+  function checkIfAllSelected() {
+    return !answers.some(it => !it)
+  }
+
   return (
-    <div>
-      <p>
-        test
-      </p>
+    <div className="container">
+      <div className="header">
+        <div>
+          <a
+            className="link"
+            href="https://engblog.ru/comparison-of-adjectives#:~:text=%D0%A1%D1%80%D0%B0%D0%B2%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%B0%D1%8F%20%D1%81%D1%82%D0%B5%D0%BF%D0%B5%D0%BD%D1%8C%20%D0%BF%D1%80%D0%B8%D0%BB%D0%B0%D0%B3%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B2%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%BC%20%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5.%20Comparative%20degree"
+            target="_blank"
+          >
+            Comparative degree
+          </a>
+
+          <a
+            className="link"
+            href="https://engblog.ru/comparison-of-adjectives#:~:text=%D0%9F%D1%80%D0%B5%D0%B2%D0%BE%D1%81%D1%85%D0%BE%D0%B4%D0%BD%D0%B0%D1%8F%20%D1%81%D1%82%D0%B5%D0%BF%D0%B5%D0%BD%D1%8C%20%D0%BF%D1%80%D0%B8%D0%BB%D0%B0%D0%B3%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D1%85%20%D0%B2%20%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%BE%D0%BC%20%D1%8F%D0%B7%D1%8B%D0%BA%D0%B5.%20Superlative%20degree"
+            target="_blank"
+          >
+            Superlative degree
+          </a>
+
+          <a
+            className="link"
+            href="https://engblog.ru/comparison-of-adjectives#:~:text=%D0%9F%D1%80%D0%B8%D0%BB%D0%B0%D0%B3%D0%B0%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5%2D%D0%B8%D1%81%D0%BA%D0%BB%D1%8E%D1%87%D0%B5%D0%BD%D0%B8%D1%8F"
+            target="_blank"
+          >
+            Exceptions
+          </a>
+        </div>
+
+        {!isAnswersVisible ? (
+          <button
+            className="button"
+            onClick={handleCheckAnswers}
+          >
+            Check Answers
+          </button>
+        ) : (
+          <button
+            className="button"
+            onClick={handleRestart}
+          >
+            Restart
+          </button>
+        )}
+
+      </div>
+
+      {TASKS.map((task, taskIndex) => {
+        let startsFrom = 0
+        if (taskIndex !== 0) {
+          startsFrom = TASKS[taskIndex - 1].questions.length
+        }
+
+        return (
+          <div key={`task-${taskIndex}`} className="content-container">
+            <span className="task-title">
+              {task.title}
+            </span>
+
+            <div className="task-container">
+              {task.questions.map((question, questionIndex) => (
+                <div key={`task-${taskIndex}-question-${questionIndex}`}>
+                  {isCheckClicked && !answers[startsFrom + questionIndex] ? (
+                    <p className="question-text question-text--highlighted">
+                      {questionIndex + 1}. {question.text}
+                    </p>
+                  ) : (
+                    <p className="question-text">
+                      {questionIndex + 1}. {question.text}
+                    </p>
+                  )}
+
+                  {question.choices.map((choice, choiceIndex) => (
+                    <div key={`task-${taskIndex}-question-${questionIndex}-choice-${choiceIndex}`}>
+                      <input
+                        type="radio"
+                        value={choice}
+                        checked={choice === answers[startsFrom + questionIndex]}
+                        // @ts-ignore
+                        onChange={e => !isAnswersVisible ? handleSetChoice(e, startsFrom + questionIndex) : () => {}}
+                      />
+                      <span className="question-choice">{choice}</span>
+                    </div>
+                  ))}
+
+                  {isAnswersVisible && (
+                    <>
+                      {answers[startsFrom + questionIndex] === question.rightAnswer ? (
+                        <p>Ura</p>
+                      ) : (
+                        <p>Ti loh</p>
+                      )}
+                    </>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/constants.ts
@@ -0,0 +1,162 @@
+export const TASKS = [
+  {
+    title: 'Choose the correct form of the comparative degree',
+    questions: [
+      {
+        text: 'I think London is more ___ than New York.',
+        choices: ['expensive', 'expensiver'],
+        rightAnswer: 'expensive',
+      },
+      {
+        text: 'In the North Sea ___ than the Mediterranean Sea?',
+        choices: ['more big', 'bigger'],
+        rightAnswer: 'more big',
+      },
+      {
+        text: 'My dad\'s ___ than your dad!',
+        choices: ['funny', 'funnier'],
+        rightAnswer: 'funnier',
+      },
+      {
+        text: 'Physics is ___ than chemistry.',
+        choices: ['more interesting', 'much interesting'],
+        rightAnswer: 'more interesting',
+      },
+      {
+        text: 'Cars are ___ than motorbikes.',
+        choices: ['much more safer', 'much safer'],
+        rightAnswer: 'much safer',
+      },
+    ]
+  },
+  {
+    title: 'Choose the correct form of the superlative degree',
+    questions: [
+      {
+        text: 'This is ___ river in Australia.',
+        choices: ['the longest', 'longest'],
+        rightAnswer: 'the longest',
+      },
+      {
+        text: 'He\'s the ___ man in the room.',
+        choices: ['taller', 'tallest'],
+        rightAnswer: 'tallest',
+      },
+      {
+        text: 'They\'re the ___ clothes I\'ve bought.',
+        choices: ['more expensive', 'most expensive'],
+        rightAnswer: 'most expensive',
+      },
+      {
+        text: 'This road is ___ in the country.',
+        choices: ['the widest', 'the wider'],
+        rightAnswer: 'the widest',
+      },
+      {
+        text: 'What is the ___ animal in the world?',
+        choices: ['small', 'smallest'],
+        rightAnswer: 'smallest',
+      },
+      {
+        text: 'Bill\'s getting married. He\'s the ___ I\'ve even seen him.',
+        choices: ['most happiest', 'happiest'],
+        rightAnswer: 'happiest',
+      },
+      {
+        text: 'This book is the  ___ book I\'ve read.',
+        choices: ['most interesting', 'much interesting'],
+        rightAnswer: 'most interesting',
+      },
+      {
+        text: 'Is the whale or the elephant the ___ animal in the world?',
+        choices: ['most big', 'biggest'],
+        rightAnswer: 'biggest',
+      },
+    ]
+  },
+  {
+    title: 'Choose the correct answer',
+    questions: [
+      {
+        text: 'This is the ___ hangover I ever had. I’m never going to drink again.',
+        choices: ['baddest', 'worst', 'best', 'least'],
+        rightAnswer: 'worst',
+      },
+      {
+        text: 'For ___ information do not hesitate to call our assistant.',
+        choices: ['farther', 'furthest', 'fastest', 'further'],
+        rightAnswer: 'further',
+      },
+      {
+        text: 'Do you know that dinosaurs were ___ than houses?',
+        choices: ['bigger', 'smaller', 'the biggest', 'the smallest'],
+        rightAnswer: 'bigger',
+      },
+      {
+        text: 'My best friend is ___ than me.',
+        choices: ['elder', 'older', 'mor older', 'the oldest'],
+        rightAnswer: 'older',
+      },
+      {
+        text: 'I’ve heard James playing the piano. He doesn’t seem to be getting ___ .',
+        choices: ['gooder', 'the best', 'better', 'the goodest'],
+        rightAnswer: 'better',
+      },
+    ]
+  },
+  {
+    title: 'Choose whether each sentence requires the comparative or superlative form',
+    questions: [
+      {
+        text: 'This is ___ book in the whole store.',
+        choices: ['more expensive', 'the most expensive'],
+        rightAnswer: 'the most expensive',
+      },
+      {
+        text: 'That shirt is ___ than the other one.',
+        choices: ['dirtier', 'the most dirty'],
+        rightAnswer: 'dirtier',
+      },
+      {
+        text: 'Peter was laughing hard, but his wife was laughing even ___!',
+        choices: ['the hardest', 'harder'],
+        rightAnswer: 'harder',
+      },
+      {
+        text: 'He is ___ man that I know.',
+        choices: ['stronger', 'the strongest'],
+        rightAnswer: 'the strongest',
+      },
+      {
+        text: 'Robert is never on time. He always arrives ___ than the rest of us.',
+        choices: ['later', 'the latest'],
+        rightAnswer: 'later',
+      },
+      {
+        text: 'That is ___ song I\'ve ever heard!',
+        choices: ['better', 'the best'],
+        rightAnswer: 'the best',
+      },
+      {
+        text: 'Your cat is ___ than ours.',
+        choices: ['smaller', 'the smallest'],
+        rightAnswer: 'smaller',
+      },
+      {
+        text: 'I thought this test would be difficult, but it\'s ___ test I\'ve ever taken.',
+        choices: ['the easiest', 'easier'],
+        rightAnswer: 'the easiest',
+      },
+      {
+        text: 'Out of all the films I\'ve seen, this one is ___.',
+        choices: ['sadder', 'the saddest'],
+        rightAnswer: 'the saddest',
+      },
+      {
+        text: 'Your answer is wrong, but it\'s ___ than the other students\' answers.',
+        choices: ['the most correct', 'more correct'],
+        rightAnswer: 'more correct',
+      }
+    ]
+  },
+]
